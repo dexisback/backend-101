a6aa4156eec17e04081b59c6e98776fa8780d0f6
Amaan <texamaan86@gmail.com>
2026-02-21 22:43:37 +0530

    fix(utils) : logic fix for asyncHandler

--- redis-and-caching/src/modules/leaderboard/leaderboard.routes.ts
+++ redis-and-caching/src/modules/leaderboard/leaderboard.routes.ts
@@ -1,13 +1,15 @@
 import { Router } from "express";
 import { leaderboardController } from "./leaderboard.controller.js";
-import asyncHandler from "../../utils/asyncHandler.js";
+import {asyncHandler} from "../../utils/asyncHandler.js";
 
-import errorMiddleware from "../../middleware/error.middleware.js";
 
 export const leaderRouter= Router();
 
 
-leaderRouter.get("/leader/me", asyncHandler(leaderboardController.register));
+leaderRouter.get("/", asyncHandler(leaderboardController.top));
+export default leaderRouter
+
+
 
 
 
