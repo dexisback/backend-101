281da73e43ca6c9e26bf8b946d01e91e0a5ede23
Amaan <texamaan86@gmail.com>
2026-02-21 22:43:10 +0530

    fix(controller) : controller uses apiresponse and safeparse

--- redis-and-caching/src/modules/leaderboard/leaderboard.controller.ts
+++ redis-and-caching/src/modules/leaderboard/leaderboard.controller.ts
@@ -11,25 +11,20 @@ import { leaderboardService } from "./leaderboard.service.js";
 export const leaderboardController = {
     //get all, 
     //GET: /leaderboard/
-    async top(req: Request, res: Response){
-        //validate query:
-        const result=QuerySchema.safeParse(req.query)
-        if(!result.success){
-            res.status(400).json({msg: "sike "})
+    async top(req: Request, res: Response){        
+        //else:
+        const data= QuerySchema.safeParse(req.query);
+        if(!data.success){
+            res.status(400).json(ApiResponse.error("bad request", 400))
             return
         }
-        //else:
-        const limit= result.data?.limit
-
-        //need to narrow limit to only number
-        if(typeof limit==="undefined"){
-                res.status(400).json({msg: "sike "})
-                return
-            
-        }
-        //call service:
+        const {limit}=data.data; //fix: earlier : const limit= data.data gave { limit } as the variable, harder type narrowing
+        //could also do : const limit=data.data.limit
+        
+        //limit needs to be narrowed down: NO, narrowing down becomes useless because safeParse ensures already that limit is a number 
+        
         const leaderboardData=await leaderboardService.gettopLeaderboard(limit)
-        return res.json({msg: leaderboardData})
+        return res.json(ApiResponse.success(leaderboardData))
     }
 }
 
