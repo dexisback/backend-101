54db827aa4e4e178b3f31b43423e53e8ff37236b
Amaan <texamaan86@gmail.com>
2026-03-11 01:46:19 +0530

    add: global error handler middleware

--- logging-metrics/src/app.ts
+++ logging-metrics/src/app.ts
@@ -4,10 +4,13 @@ import type { NextFunction, Request, Response } from "express";
 import { requestLogger } from "./middleware/requestLogger.middleware.js";
 import { logger } from "./lib/logger.js";
 import responseTime from "response-time";   //external lib to calculate response time 
+import { globalErrorHandler } from "./middleware/globalErrorHandler.middleware.js";
 const app =express();
 app.use(express.json());
 
 app.use(responseTime());
+//global erorr handler middleware:
+app.use(globalErrorHandler)
 //request logger middleware:
 app.use(requestLogger)
 
