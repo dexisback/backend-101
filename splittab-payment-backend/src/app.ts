eaf02fa2bcfffd69620a9f34208e590ecceb402b
Amaan <texamaan86@gmail.com>
2026-03-30 00:20:32 +0530

    using prisma

--- splittab-payment-backend/src/app.ts
+++ splittab-payment-backend/src/app.ts
@@ -1,10 +1,2 @@
-import { Router } from "express";
-import tabsRoutes from "./modules/tabs/tabs.routes.js"
-
-const router = Router();
-
-//----
-
-
-router.use("/tabs", tabsRoutes);
-export default router;
+import express from "express"
+import tabsRoutes from 
