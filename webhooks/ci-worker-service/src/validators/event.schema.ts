de2be48c42564f22ce2bfb1ac2b4d57ff9362b22
Amaan <texamaan86@gmail.com>
2026-03-30 05:33:13 +0530

    add: event validation schema
    do not trust project 1 input blindly

---
+++ webhooks/ci-worker-service/src/validators/event.schema.ts
@@ -0,0 +1,16 @@
+import z from "zod";
+
+export const githubPushEventSchema = z.object({
+    type: z.literal("github.push"),
+    payload: z.object({
+        repo: z.string().min(1),
+        commitId: z.string().min(1),
+        branch: z.string().min(1),
+        author: z.string().min(1)
+    })
+})
+
+
+export const eventhSchema = z.discriminatedUnion("type", [githubPushEventSchema])
+
+export type Event = z.infer<typeof eventhSchema>
