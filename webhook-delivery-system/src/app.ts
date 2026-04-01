de34bdf770bdbbc7991e0e6ffa444c48705a6898
Amaan <texamaan86@gmail.com>
2026-04-02 01:22:12 +0530

    dlt trash

--- webhook-delivery-system/src/app.ts
+++ webhook-delivery-system/src/app.ts
@@ -18,5 +18,6 @@ app.register(eventRoutes)
 //self notes: 
 //express vs fastify -> route registration , in express: app.post("/subscription", handler) in fastify app.post("/subscription", async (request, reply)=>{}) so you dont need any handler and usme async bananna=> you directly can add the logic in there 
 //no more res.status().json()  -> res.status().send() or just return data     -> fastify auto handles serialisation
+//worker is a separate process, runs separately
 
 
