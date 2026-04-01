397dbdadce6dd126e3d87a832bf46780cf1ade25
Amaan <texamaan86@gmail.com>
2026-04-02 01:22:00 +0530

    add: webhookWorker

---
+++ webhook-delivery-system/src/modules/delivery/webhookDelivery.worker.ts
@@ -0,0 +1,28 @@
+import { Worker } from "bullmq";
+import { env } from "../../config/env.js";
+
+
+export const webhookDeliveryWorker = new Worker(
+  "delivery-queue",
+  async (job) => {
+    const { eventId, subscriptionId, payload } = job.data;
+
+    console.log("Processing job:", {
+      eventId,
+      subscriptionId,
+    });
+
+    // TODO:
+    // fetch event + subscription from DB
+    // send webhook
+    // log result
+  },
+  {
+    connection: {
+      url: env.REDIS_URL!,
+    },
+  }
+);
+
+
+
