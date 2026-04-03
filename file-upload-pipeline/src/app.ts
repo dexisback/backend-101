3969a39c002f25995b84d850dfa27022b4f24772
Amaan <texamaan86@gmail.com>
2026-04-03 18:02:43 +0530

    add: ;arge file pipeline schema

--- file-upload-pipeline/src/app.ts
+++ file-upload-pipeline/src/app.ts
@@ -1,13 +1,11 @@
 import express from "express";
 import type { Request, Response, Application } from "express";
-import { Router } from "express";
 import mediaRoutes from "./modules/media.routes.js"
 import { globalErrorHandler } from "./middleware/error.js";
 const app: Application = express();
 
 
 app.use(express.json({limit: "10kb"}));
-
 app.use(express.urlencoded({ extended: true, limit: "10kb"}))  //idk man, funnily enough i learnt this loc in my web dev class in college(they teach ajax and jquery)
 
 
@@ -19,7 +17,9 @@ app.get("/health", (_req: Request, res: Response)=>{
     })
 })
 
+
 app.use("/api/media", mediaRoutes)
+//app.use("/api/webhooks", webhookRoutes) //cloudinary calls this endpoint
 
 
 app.use(globalErrorHandler)
