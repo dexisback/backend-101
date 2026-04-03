84729cfde0c6774b4436d5d45c9b4b74702d6379
Amaan <texamaan86@gmail.com>
2026-04-04 00:08:15 +0530

    sorry, re-doing

--- splittab-payment-backend/src/modules/tabs/tabs.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-import * as tabsController from "./tabs.controller.js"
-const router= Router();
-
-
-//POST /tabs:
-router.post("/", tabsController.createTab);  //so /tabs/  will now create a tab 
-export default router 
