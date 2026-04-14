2b349679626eb91722978341752ff4da9483b0e7
Amaan <texamaan86@gmail.com>
2026-04-15 01:21:00 +0530

    fix: cloudinary docs matching variables (timeStamp -> timestamp, allowed_formats)

--- file-upload-pipeline/src/services/signature.service.ts
+++ file-upload-pipeline/src/services/signature.service.ts
@@ -1,6 +1,5 @@
 //meant for large files:
 
-import { timeStamp } from "node:console"
 import cloudinary from "../config/cloudinary.js"
 import { env } from "../config/env.js"
 
@@ -14,13 +13,12 @@ interface signatureParams {
 export const secureSignatureGenerator = ({timeStamp, folder, mediaId}: signatureParams) =>{
     //define the strict rules the upload MUST follow:
     const uploadParams = {
-        timeStamp,
+        timestamp: timeStamp,
         folder,
-        allowedFormats : ['mp4', 'mov', 'webm'], //cloudinary rejects anything else
-        context: `mediaId = ${mediaId}`     // Hidden metadata for our Webhook
+        allowed_formats : ['mp4', 'mov', 'webm'], //cloudinary rejects anything else
+        context: `mediaId=${mediaId}`     // Hidden metadata for our Webhook
 
     }
     const signature = cloudinary.utils.api_sign_request(uploadParams, env.CLOUDINARY_API_SECRET)
     return { signature, uploadParams }
 }
-
